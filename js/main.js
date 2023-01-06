7b58793e528841eadb478a47b50bc3860cb72ded
Pedro Germano <pedrogermano232@gmail.com>
2023-01-06 18:52:34 -0300

    media querie

--- js/main.js
+++ js/main.js
@@ -2,12 +2,12 @@ function toggleMode() {
   const html = document.documentElement
   html.classList.toggle('light')
 
-  const img = document.querySelector('#profile img')
+  const img = document.querySelector('.profile img')
 
   
   if(html.classList.contains('light')) {
     img.setAttribute('src', './img/profile-day.png')
-    
+
   } else {
     img.setAttribute('src', './img/profile.png');
   }
